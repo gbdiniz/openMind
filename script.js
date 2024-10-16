22180daca70829b50d123a0c2bc717bb432d12cf
Gabriel Diniz dos Santos <gabrieldiniz.profissional@gmail.com>
2024-10-15 22:50:07 -0300

    Responsividade. Avatares na third-content.

--- script.js
+++ script.js
@@ -20,3 +20,51 @@ window.onscroll = function () {
         header.classList.remove("bg-header");
     }
 }
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+        if (entry.isIntersecting && entry.target.id == 'first-content') {
+            // Add the animation class
+            entry.target.classList.add('right-slidein-animation');
+        }
+
+        // If the element is visible
+        if (entry.isIntersecting && entry.target.classList.contains("about-person")) {
+            // Add the animation class
+            entry.target.classList.add('top-slidein-animation');
+        }
+
+        if (entry.isIntersecting && entry.target.id == 'third-content') {
+            // Add the animation class
+            const children = entry.target.children;
+
+            children[0].classList.add('right-slidein-animation');
+            children[1].classList.add('left-slidein-animation');
+        }
+
+        if (entry.isIntersecting && entry.target.id == 'fourth-content') {
+            // Add the animation class
+            const children = entry.target.children;
+
+            children[0].classList.add('top-slidein-animation');
+
+            // children[1].classList.add('left-slidein-animation')
+        }
+    });
+});
+
+observer.observe(document.querySelector('#first-content'));
+
+const avatars = document.querySelectorAll('.about-person');
+
+// Loop over the elements and add each one to the observer
+avatars.forEach((element) => observer.observe(element));
+
+const thirdContent = document.querySelectorAll('#about-person');
+
+thirdContent.forEach((element) => observer.observe(element));
+
+observer.observe(document.querySelector('#third-content'));
+
+observer.observe(document.querySelector('#fourth-content'));
