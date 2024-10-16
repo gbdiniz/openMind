277c3ab5f4a29ed88631e1cb046b4b800b49b0f1
Gabriel Diniz dos Santos <gabrieldiniz.profissional@gmail.com>
2024-10-16 15:44:46 -0300

    Ajustes.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Animação de opacidade do body ao carregar a página
 document.body.onload = () => {
     document.body.style.opacity = '1'
-    // document.getElementById('header').style.top = 0
 };
+
+// Função ao clicar o botão, navegar para a seção desejada
 document.addEventListener('click', function (event) {
     if (!event.target.matches('.nav-link') && !event.target.matches('.our-games')) return;
 
@@ -12,6 +14,8 @@ document.addEventListener('click', function (event) {
     element.scrollIntoView({ behavior: "smooth", block: "center" });
 
 });
+
+// Header muda o background ao rolar para o início ou para baixo
 window.onscroll = function () {
     var header = document.getElementById('header');
     if (window.pageYOffset > 50) {
@@ -21,50 +25,43 @@ window.onscroll = function () {
     }
 }
 
+// Contrução da função para observar cada seção e então adicionar a animação
 const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
+    // Para cada seção que é carregada ao rolar a página
     entries.forEach(entry => {
         if (entry.isIntersecting && entry.target.id == 'first-content') {
-            // Add the animation class
             entry.target.classList.add('right-slidein-animation');
         }
 
-        // If the element is visible
         if (entry.isIntersecting && entry.target.classList.contains("about-person")) {
-            // Add the animation class
             entry.target.classList.add('top-slidein-animation');
         }
 
-        if (entry.isIntersecting && entry.target.id == 'third-content') {
-            // Add the animation class
-            const children = entry.target.children;
+        if (entry.isIntersecting && entry.target.id == 'game1') {
+            entry.target.classList.add('right-slidein-animation');
+        }
 
-            children[0].classList.add('right-slidein-animation');
-            children[1].classList.add('left-slidein-animation');
+        if (entry.isIntersecting && entry.target.id == 'game2') {
+            entry.target.classList.add('left-slidein-animation');
         }
 
         if (entry.isIntersecting && entry.target.id == 'fourth-content') {
-            // Add the animation class
             const children = entry.target.children;
-
             children[0].classList.add('top-slidein-animation');
-
-            // children[1].classList.add('left-slidein-animation')
         }
     });
 });
 
+// Observador da seção 1 do site
 observer.observe(document.querySelector('#first-content'));
 
+// Observador da seção "Nossa equipe" do site
 const avatars = document.querySelectorAll('.about-person');
-
-// Loop over the elements and add each one to the observer
 avatars.forEach((element) => observer.observe(element));
 
-const thirdContent = document.querySelectorAll('#about-person');
-
+// Observador da seção "Nossos jogos" do site
+const thirdContent = document.querySelectorAll('.row');
 thirdContent.forEach((element) => observer.observe(element));
 
-observer.observe(document.querySelector('#third-content'));
-
+// Observador da seção "Contato" do site
 observer.observe(document.querySelector('#fourth-content'));
